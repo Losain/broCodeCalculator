fa47d78508c338e1b32dc56a92e66d5d1bd355dd
Losain <nate.clawson04@gmail.com>
2025-10-26 11:53:57 -0600

    Add project files.

---
+++ broCodeCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace broCodeCalculator
+{
+    public class MyClass
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            double num1;
+            double num2;
+            string opratr;
+            bool calcAgain = true;
+            //we want to continue calculating until we are told to exit.
+            while (calcAgain)
+            {
+                //ask for input
+                Console.WriteLine("welcome to calculator");
+                Console.Write("please enter your first number: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("First number is: " + num1);
+                Console.WriteLine("Please enter you second number: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Second number is: " + num2);
+
+
+            }
+
+            //prevent the program from closing
+            Console.ReadLine();
+        }
+    }
+}
