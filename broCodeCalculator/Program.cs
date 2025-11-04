30b5285239f5ee39f9154cd8a322c9a248c85a48
Losain <nate.clawson04@gmail.com>
2025-11-03 19:33:38 -0700

    Adding in more unit tests and making a few changes to the num1

--- broCodeCalculator/Program.cs
+++ broCodeCalculator/Program.cs
@@ -12,18 +12,6 @@ namespace broCodeCalculator
 
         static void Main()//didn't need arg
         {
-            //declare variables. inefficient. may not actually use. 
-            // double num1;
-            //double num2;
-            //string opratr;
-            //bool calcAgain = true;
-            //string input;
-            //Match match;
-            //we want to continue calculating until we are told to exit.
-
-            //lets try to mess with REGEX and parsing the string. 
-            //string pattern = @"^([\d\.]+)\s*([-+*/])\s*([\d\.]+)$"; //I did a bad and copied this regex from the internet. (?:) == dosn't capture group. 
-            //regex need anchors. 
 
             while (true)//Loop exiting is handled in conditions. I KNOW WHAT I DID. 
             {
@@ -56,12 +44,21 @@ namespace broCodeCalculator
                 throw new ArgumentException($"'{input}' is not properly formated.");
             }
             //variables string TODO. 
-            bool num1Converted = double.TryParse(match.Groups[1].Value, out double num1);
-            bool num2Converted = double.TryParse(match.Groups[3].Value, out double num2);
-            if (!num1Converted)//"failing faster sorta (not technically)
+            //bool num1Converted = true;
+            double num1;
+            try
+            {
+                num1 = double.Parse(match.Groups[1].Value);
+            }
+            catch (FormatException)
             {
                 throw new ArgumentException($"'{match.Groups[1].Value}' is not a number.");
             }
+
+
+
+            bool num2Converted = double.TryParse(match.Groups[3].Value, out double num2);
+          
             if (!num2Converted)//"failing faster sorta (not technically)
             {
                 throw new ArgumentException($"'{match.Groups[3].Value}' is not a number.");
--- xUnit_broCodeCalculator/UnitTest1.cs
+++ xUnit_broCodeCalculator/UnitTest1.cs
@@ -19,5 +19,30 @@ namespace xUnit_broCodeCalculator
             double actual = broCodeCalculator.MyClass.Evaluate(input);
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void SadPath()
+        {
+            string input = "hihowareyeah";
+            ArgumentException e = Assert.Throws<ArgumentException>(() => broCodeCalculator.MyClass.Evaluate(input));//$"'{input}' is not properly formated."
+            Assert.Equal($"'{input}' is not properly formated.", e.Message);
+        }
+
+        [Theory]
+        [InlineData("1.1.2+2", "'1.1.2' is not a number.")] //$"'{match.Groups[1].Value}' is not a number."
+        [InlineData("2+2.2.3", "'2.2.3' is not a number.")]
+        [InlineData("", "'' is not properly formated.")]
+        //[InlineData(null, "'' is not properly formated.")] null is not captured. 
+
+        //can't/don't overload unit toast. 
+        public void NumberEval(string input, string expected)
+        {
+            ArgumentException e = Assert.Throws<ArgumentException>(() => broCodeCalculator.MyClass.Evaluate(input));
+            Assert.Equal(expected, e.Message);
+        }
+
+        //Fact for null arg exception.
+        //white space
+        //random string 
     }
 }
